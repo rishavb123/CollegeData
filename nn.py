910e953c128e257ae5c5a9c3708b653493314cdd
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-10-22 17:20:23 -0400

    created basic neural net library

---
+++ nn.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+class NeuralNetwork:
+    
+    relu = lambda x: x * (x > 0)
+    sigmoid = lambda x: 1 / (1 + np.exp(-x))
+
+    def __init__(self, shape, activation=sigmoid, learning_rate=0.1):
+        self.shape = shape
+        self.activation = np.vectorize(activation)
+        self.activation_derivative = np.vectorize(NeuralNetwork.derivative(activation))
+
+        self.weights = []
+        self.biases = []
+
+        for i in range(len(self.shape) - 1):
+            self.weights.append(np.random.random([self.shape[i + 1], self.shape[i]]))
+            self.biases.append(np.random.random([self.shape[i + 1], 1]))
+
+        self.learning_rate = learning_rate
+
+    def predict(self, inp):
+        a = np.array(inp)
+        for weight, bias in zip(self.weights, self.biases):
+            a = self.activation(np.matmul(weight, a) + bias)
+        return a
+
+    def train(self, inp, target):
+        target = np.array(target)
+        a = [np.array(inp)]
+        z = [a[0]]
+        i = 0
+        for weight, bias in zip(self.weights, self.biases):
+            i += 1
+            z.append(np.matmul(weight, a[len(a) - 1]) + bias)
+            a.append(self.activation(z[len(z) - 1]))
+        output = a[len(a) - 1]
+        errors = [None for _ in range(len(self.shape))]
+        errors[len(errors) - 1] = target - output
+
+        delta_weights = [None for _ in self.weights]
+        delta_biases = [None for _ in self.biases]
+
+        for i in range(len(self.weights), 0, -1):
+            errors[i - 1] = np.matmul(np.transpose(self.weights[i - 1]), errors[i])
+            temp = errors[i] * self.activation_derivative(z[i])
+            # print("biases", np.shape(self.biases[i - 1]), "weights", np.shape(self.weights[i - 1]), "temp", np.shape(temp), "a", np.shape(a[i - 1]))
+            delta_weights[i - 1]= np.matmul(temp, np.transpose(a[i - 1])) * self.learning_rate
+            delta_biases[i - 1] = temp * self.learning_rate
+            self.weights[i - 1] += delta_weights[i - 1]
+            self.biases[i - 1] += delta_biases[i - 1]
+
+    def predict_set(self, inputs):
+        return [self.predict(inp) for inp in inputs]
+
+    def train_set(self, inputs, targets, epoch=1):
+        for i in range(epoch):
+            for inp, target in zip(inputs, targets):
+                self.train(inp, target)
+
+    @staticmethod
+    def derivative(f, h=0.0001):
+        return lambda x: (f(x + h / 2) - f(x - h / 2)) / h
