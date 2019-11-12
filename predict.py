967a79066b442157abb6739896f3354f820060e3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-11-12 14:12:09 -0500

    changed terminology

--- predict.py
+++ predict.py
@@ -8,5 +8,5 @@ with open(model_file, 'rb') as f:
     model = pickle.load(f)
     while ask.lower() == 'y':
         y = model.predict([input("Type: "), input("GPA: "), input("SAT: "), input("ACT: "), input("Year: ")])
-        print('Your getting', y[0], 'with a', int(np.round(y[1] * 100)), 'percent probability')
+        print('Your getting', y[0], 'with a', int(np.round(y[1] * 100)), 'percent confidence')
         ask = input('Again? (Y/n): ')
